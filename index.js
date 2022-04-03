d17b6fac09d70853d8ed2bdd9a5ff0cf980febaf
katariarajat <katariarajat999@gmail.com>
2022-04-03 23:51:08 +0530

    cors

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.use(bodyParser.json());
 // auth check
 app.use(isAuth);
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
